4ca48e22e37b961f4f6784cabf55dde10c7e9b5f
Erick <eum602@gmail.com>
2019-09-21 11:24:22 -0500

    CONTROL FLOW - explained in GO

---
+++ 25_helloWorld/helloWorld.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+	foo()
+	fmt.Println("After foo")
+
+	for i:=0; i <100 ; i++ {
+		if i%50==0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func  foo()  {
+	fmt.Println("I am in foo")
+}
+
+func bar()  {
+	fmt.Println("Finishing with bar; all this is the control flow in go, I mean how statements and declarations are analyzed and executed")
+}
