8c5ac4ace15d5dbd6fa0ba561c6f092e66d4ecd1
Erick <eum602@gmail.com>
2019-12-01 11:35:01 -0500

    basic declaration for constants

--- 40_types/numericTypes.go
+++ 40_types/numericTypes.go
@@ -1,58 +1,59 @@
 package main
-import "fmt"
-import "runtime"
+
+import (
+	"fmt"
+	"runtime"
+)
 
 var a int
 var b float32
 
-func main(){
+func main() {
 	//find all about numerics here: https://golang.org/ref/spec#Numeric_types
 	//a=42.456 //this is not going to run because a is previously declared as int
-	a=42
-	b=42.56
+	a = 42
+	b = 42.56
 
-	fmt.Println("a variable is",a, "its type is")
-	fmt.Printf("%T\n",a)
-	fmt.Println("b variable is",b,"its type is")	
-	fmt.Printf("%T\n",b)
+	fmt.Println("a variable is", a, "its type is")
+	fmt.Printf("%T\n", a)
+	fmt.Println("b variable is", b, "its type is")
+	fmt.Printf("%T\n", b)
 
 	var c int8 //-128 ==> 127
 	//c=-129 //this is not going to work
-	fmt.Println("c variable is",c,"its type is")	
-	fmt.Printf("%T\n",c)
+	fmt.Println("c variable is", c, "its type is")
+	fmt.Printf("%T\n", c)
 
 	//ALIASES:
 	/*
 	* byte: uint8
 	* rune: int32
-	*/
+	 */
 
-	fmt.Println("Using runtime package to get the OS",runtime.GOOS)
+	fmt.Println("Using runtime package to get the OS", runtime.GOOS)
 	fmt.Println("Using runtime package to print the running program architecture", runtime.GOARCH)
 
 	//********************STRINGS*****************************//
 	fmt.Println("**********************************************")
 	fmt.Println("****************STRINGS***********************")
-	s:= "Hello this is nome non useful text"
+	s := "Hello this is nome non useful text"
 	fmt.Println(s)
-	fmt.Printf("%T\n",s)
-	
-	u:=`This is a text created with backticks`
+	fmt.Printf("%T\n", s)
+
+	u := `This is a text created with backticks`
 
 	fmt.Println(u)
 
-
-	bs:=[]byte(s) //using conversion to convert from type string to a slice of bytes(uint8)
+	bs := []byte(s) //using conversion to convert from type string to a slice of bytes(uint8)
 	//basically it convert each letter to the correspondent ascii: http://sticksandstones.kstrom.com/appen.html
 
-	
-	fmt.Println("The conversion from type string to a slice of bytes is",bs)
+	fmt.Println("The conversion from type string to a slice of bytes is", bs)
 	fmt.Println("Type is:") //[]uint8 ;
-	fmt.Printf("%T\n",bs)
+	fmt.Printf("%T\n", bs)
 
 	/* **********************************************************
-	
-	*/
+
+	 */
 	fmt.Println("****************Ways to print Strings with fmt***********************")
 	message := `Ways to print Strings with fmt package; 
 	%s	the uninterpreted bytes of the string or slice
@@ -64,14 +65,14 @@ func main(){
 	fmt.Println(message)
 
 	fmt.Println("\nPrinting utf characters for a text:")
-	for i :=0; i < len(s); i++ {
-		fmt.Printf("%#U ",s[i])//each utf code is expresed in int32 (rune)
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%#U ", s[i]) //each utf code is expresed in int32 (rune)
 	}
 
 	fmt.Println("\n\nShowing the text in their correspondent bytes(uint8) in HEXADECIMAL format:")
 
-	for i, v:=range s{
-		fmt.Printf("At index position %d we have the %#x\n",i,v)//%d ==> base 10; %x prints without leading format (0b
+	for i, v := range s {
+		fmt.Printf("At index position %d we have the %#x\n", i, v) //%d ==> base 10; %x prints without leading format (0b
 		//for  binary; 0x for hexadecimal and so on); if we add # (eg %#x) then it will print with the leading format.
 		//read fmt package from godoc.org/fmt
 	}
@@ -82,4 +83,13 @@ func main(){
 	fmt.Println(s)
 
 	//adittional resource to understand strings in : https://blog.golang.org/strings
+
+	//***********************constants***********************
+	//https://golang.org/ref/spec
+	const w1 = 42
+	const w2 = 42.78
+	const w3 = "something"
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
 }
