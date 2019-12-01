84582b672b4e9daeb09e7765b6e0bbbe2706f8dc
Erick <eum602@gmail.com>
2019-12-01 12:49:44 -0500

    added equivalences between kb, mb, and gb

--- 40_types/bitShifting.go
+++ 40_types/bitShifting.go
@@ -11,4 +11,14 @@ func main() {
 
 	w := x << 2 //bitshifting 11 ===> 1100 (increments 2 zeroes in the right hand side)
 	fmt.Printf("%d\t\t%b\n", w, w)
+
+	fmt.Println("\nPrinting equilalences between kilobytes, megabytes and gigiabytes")
+
+	kb := 1024 //kilobyte is 1024 bytes
+	mb := 1024 * kb
+	gb := 1024 * mb
+
+	fmt.Printf("%d\t\t\t%b\n", kb, kb)
+	fmt.Printf("%d\t\t\t%b\n", kb, mb)
+	fmt.Printf("%d\t\t\t%b\n", mb, gb)
 }
