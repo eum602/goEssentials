ce8e69032d4fb562d2d9eee4616871d4db2f326a
Erick <eum602@gmail.com>
2019-12-31 23:10:38 -0500

    Added new function which defines PARAMETERS and thus receives ARGUMENTS

--- 107_syntax/main.go
+++ 107_syntax/main.go
@@ -6,6 +6,8 @@ import (
 
 func main() {
 	foo() //no ARGUMENTS are passed in this example
+	//Everything in GO is PASS BY VALUE
+	bar("Erick") //Passing the ARGUMENT "Erick"
 }
 
 func foo() {
@@ -13,4 +15,8 @@ func foo() {
 	func receiver(r receiver) identifier(parameters) (returns(s)){...}
 	Note: Recall that when you define a function we say PARAMETERS(if any), but when you CALL A FUNCTION you pass ARGUMENTS
 	`)
+}
+
+func bar(s string) {
+	fmt.Println(`This is another function with PARAMETER 's'   :`, s)
 }
