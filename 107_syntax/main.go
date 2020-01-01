f7764b9fcdf69859de16d4dd8d320a3cb9a1006a
Erick <eum602@gmail.com>
2019-12-31 23:02:27 -0500

    created a simple function which does not pass PARAMETERS

--- 107_syntax/main.go
+++ 107_syntax/main.go
@@ -5,7 +5,12 @@ import (
 )
 
 func main() {
+	foo() //no ARGUMENTS are passed in this example
+}
+
+func foo() {
 	fmt.Println(`Generalization of function:
 	func receiver(r receiver) identifier(parameters) (returns(s)){...}
+	Note: Recall that when you define a function we say PARAMETERS(if any), but when you CALL A FUNCTION you pass ARGUMENTS
 	`)
 }
