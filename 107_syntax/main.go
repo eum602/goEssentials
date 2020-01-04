5cfe36e7acd9fecaa98ebcb12511c5468510d76a
Erick <eum602@gmail.com>
2020-01-04 15:16:02 -0500

    added DEFER keywork and explained how it delays the execution of a function to the end of the execution

--- 107_syntax/main.go
+++ 107_syntax/main.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+	defer sumInts("This text shoud be shown at the end of this final go program because a 'defer' keyword is deferring its execution")
 	foo() //no ARGUMENTS are passed in this example
 	//Everything in GO is PASS BY VALUE
 	bar("Erick") //Passing the ARGUMENT "Erick"
@@ -19,7 +20,6 @@ func main() {
 	xi := []int{1, 2, 3, 4, 5, 6, 7}
 	sum = sumInts("some text", xi...)
 	fmt.Println("Print sum after spread slice was passed", sum)
-	sumInts("another some text")
 }
 
 func foo() {
@@ -30,6 +30,11 @@ func foo() {
 		func some(a int, s ...string).
 	* If no variadic argument is passed the slice will still be created BUT as "nil" and the UNDERLYING array would have not been
 	created, so the slice exits but the pointer is pointing to nothing
+	* Defer keyword: https://golang.org/ref/spec#Keywords => invokes a function whose execution is deferred to the moment the
+	 surrounding function returns:
+			 either because the surrounding function executed a return statement, 
+			 reached the end of its function body, 
+			 or because the corresponding goroutine is panicking.
 	`)
 }
 
@@ -48,6 +53,7 @@ func greeting(fn string, ln string) (string, bool) {
 func sumInts(s string, x ...int) int { //declaring VARIADIC PARAMETERS, in this way it is possible to declare many paramenters
 	//then when received, those parameters will be stored as a SLICE.
 	//variadic functions like this can receive ZERO or more arguments.
+	println("\n\n", s)
 	println("===================SUMS===================")
 	fmt.Println("content of variadic slice is", x, "and the type of that variadic is:")
 	fmt.Printf("%T\n", x)
