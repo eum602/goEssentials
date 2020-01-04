629c7764ec9d400494a92ae02b5f72cab6dcb345
Erick <eum602@gmail.com>
2020-01-04 14:20:28 -0500

    explained append method in slices

--- 78_slices/main.go
+++ 78_slices/main.go
@@ -12,6 +12,8 @@ func main() {
 	s := []int{3, 5, 6, 45, 256, 76, 4}
 	getContentAtIndexOfSlice(s, 2)
 	sliceASlice(s, 3, 5)
+	appendToSliceExamples(s)
+
 }
 
 func sliceDefinition() {
@@ -52,4 +54,15 @@ func sliceASlice(s []int, from, upToButNotIncluding int) {
 	fmt.Println("s[:]  => ", s[:])
 	fmt.Println("s[2:] => ", s[2:])
 	fmt.Println("s[:4] => ", s[:4])
+}
+
+func appendToSliceExamples(s []int) {
+	values := []int{0, 4, 79}
+	fmt.Println(`using '...' to decompose the input (in case it is another slice ==> s1 := append(s, values...) ==>`)
+	s1 := append(s, values...)
+	fmt.Println("input => s=", s, "elements to append", values, `
+	s + appended elements => s1 := append(s, values...) =  `, s1)
+	s2 := append(s, 1, 4, 5)
+	fmt.Println(`A simple way to append is by using something like this: 
+	s = append(s,1,4,5)= `, s2)
 }
