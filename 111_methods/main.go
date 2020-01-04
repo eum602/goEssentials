97d23d2583c8138a0c2a4e067be4291615ff9f97
Erick <eum602@gmail.com>
2020-01-04 17:49:41 -0500

    created METHODS which are ATTACHED to STRUCTS ==> 'func (s secretAgent) speak()' ==> 'sa1.speak()'

--- 111_methods/main.go
+++ 111_methods/main.go
@@ -15,6 +15,8 @@ type secretAgent struct {
 }
 
 func main() {
+	fmt.Println(`Definition of function: 
+	func (r receiver) identifier(parameters) (returns s){...}`)
 	sa1 := secretAgent{
 		person: person{
 			"Erick",
@@ -23,5 +25,21 @@ func main() {
 		ltk: true,
 	}
 
+	sa2 := secretAgent{
+		person: person{
+			"John",
+			"Doe",
+		},
+		ltk: true,
+	}
+
 	fmt.Println(sa1)
+	sa1.speak()
+	sa2.speak()
+}
+
+func (s secretAgent) speak() {
+	fmt.Println(`This function can only be accessed by secretAgent type, in other words this function has been 
+	ATTACHED to secretAgent type, so any value of type secretAgent can access here`)
+	fmt.Println(`I'm `, s.first, s.last)
 }
