ad778aaec65aa844403500e3c4c7ee428d815f58
Erick <eum602@gmail.com>
2020-01-05 17:53:19 -0500

    added an additional GOROUTINE by using the keyword 'go' before calling a function, proved # of goroutines have increased by using package 'runtime' => runtime.NumGoroutine()

--- 148_wait_group/main.go
+++ 148_wait_group/main.go
@@ -6,13 +6,25 @@ import (
 )
 
 func main() {
-	getEnv()
+	getEnv() //gooroutines=1
+	go foo() //the code when added keyword "go" doesn't get printed nothing to the console
+	getEnv() //goroutines=2
 
 }
 
 func getEnv() {
+	fmt.Println("======= Go runtime =======")
 	fmt.Println("OS\t ", runtime.GOOS)
 	fmt.Println("ARCH\t ", runtime.GOARCH)
 	fmt.Println("CPUs\t ", runtime.NumCPU())
 	fmt.Println("Goroutines\t ", runtime.NumGoroutine())
+	if runtime.NumGoroutine() > 1 {
+		fmt.Println("At this point there is concurrent code up and running")
+	}
+}
+
+func foo() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("num is", i)
+	}
 }
