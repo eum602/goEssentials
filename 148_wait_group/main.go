a949254131e7ce2d16657e57c7fc951ca55b960f
Erick <eum602@gmail.com>
2020-01-05 20:01:49 -0500

    added synchronization through the use of WAITGROUP (add, wait and dome))==> now main waits until concurrent methods finishes, also now concurrent prints can be shown

--- 148_wait_group/main.go
+++ 148_wait_group/main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
+var wg sync.WaitGroup //created a variable of type waitGroup
+
 func main() {
-	getEnv() //gooroutines=1
-	go foo() //the code when added keyword "go" doesn't get printed nothing to the console because it will
+	getEnv()  //gooroutines=1
+	wg.Add(1) //There is one thing I am waiting for, so I add a thing to wait
+	go foo()  //the code when added keyword "go" doesn't get printed nothing to the console because it will
 	//be run on another routine thanks to keyword 'go' , also as this function is into main, then when main finishes
 	//then all into it is forced to finish even when it has not finished, so foo is forced to terminate in case it is
 	//still running ==> so to avoid that behavior a some sort of synchronization is needed.
-	getEnv() //goroutines=2
+	//WaitGroups is one option for the synchronization:
+	getEnv()  //goroutines=2
+	wg.Wait() //It will prevent the main function to finish until receiving the done signal
 
 }
 
@@ -30,4 +36,6 @@ func foo() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("num is", i)
 	}
+
+	wg.Done() //As the function has finished then done indicates to finish and remove things added
 }
