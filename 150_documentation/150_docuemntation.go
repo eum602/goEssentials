1c085eb7cc64a87bcfac9d048c286f5b773e456c
Erick <eum602@gmail.com>
2020-01-27 23:41:07 -0500

    added example of race condition

--- 150_documentation/150_docuemntation.go
+++ 150_documentation/150_docuemntation.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 )
 
 func main() {
-	definitions()
+	//definitions()
+	example1()
 }
 
 func definitions() {
@@ -21,3 +24,36 @@ func definitions() {
 	
 	Function literals are closures`)
 }
+
+func example1() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine()) //GOMAXPROCS ==> number of maximum goroutines that can be executing simultaneously, by default
+	//it is set to the max number of cores available
+	counter := 0
+	var wg sync.WaitGroup
+	const gs = 100
+	wg.Add(gs)
+	for i := 0; i < gs; i++ {
+		go func() { //launching a hundred of goroutines to simulate race condition
+			v := counter //reading a value
+			//time.Sleep(time.Second) //sleep the goroutine
+			runtime.Gosched() //contributes to race by allowing something else to run, this is the breaking point to
+			//simulate the race condition
+			v++
+			counter = v //writing a value to the shared variable
+			wg.Done()
+		}()
+
+		fmt.Println("Goroutines:", runtime.NumGoroutine())
+
+	}
+
+	wg.Wait()
+
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("count", counter)
+
+}
+
+//run with go run -race *.go ==> for more info see go help build
+//Found 1 data race(s)
