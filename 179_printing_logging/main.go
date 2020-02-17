2017a9694b882bf48f44affbe3dcb689ba740c89
Erick <eum602@gmail.com>
2020-02-17 23:11:55 +0000

    simulated file open error in order to write to a log file through log.Println(...)

--- 179_printing_logging/main.go
+++ 179_printing_logging/main.go
@@ -17,4 +17,11 @@ func example1() {
 	}
 	defer f.Close()  //closing at the end
 	log.SetOutput(f) //setting that all log information input will go to the log.txt file
+
+	//Now simulating an error to write it to a file
+	f2, err := os.Open("non-existent-file.txt")
+	if err != nil {
+		log.Println("An error has happened: ", err) //this log will be written to log.txt
+	}
+	defer f2.Close()
 }
