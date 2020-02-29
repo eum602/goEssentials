c04890d4995b6ccdd16f1c0228b786335fa7a4a4
Erick <eum602@gmail.com>
2020-02-29 10:51:31 -0500

    added unmarshall examples, in this case the unmarshall receives an array of bytes and decodes it into some specified tyoe

--- 135_json_documentation/main.go
+++ 135_json_documentation/main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	definitions()
 	marshallExample1()
+	unMarshallExample1()
+	unMarshallExample2()
 }
 
 func definitions() {
@@ -16,6 +18,7 @@ func definitions() {
 	https://golang.org/ ==> Package Documentation ==> encoding ==> json
 	https://godoc.org/encoding/json ==> marshal ==> in that example a struct is taken into a json
 	https://godoc.org/encoding/json ==> unmarshaling ==> takes []byte(...{json object}...) ==> into  a desired value
+	https://mholt.github.io/json-to-go/ ==> resource to autogenerate go code from json
 	`)
 }
 
@@ -41,6 +44,35 @@ func marshallExample1() []byte {
 	if err != nil {
 		fmt.Println("Err", err)
 	}
+	fmt.Println("\n\n****Unmarshall Examples***")
 	os.Stdout.Write(b)
 	return b
+}
+
+func unMarshallExample1() {
+	b := marshallExample1()
+	fmt.Println(b)
+}
+
+func unMarshallExample2() {
+	var myJSON = []byte(`[
+		{"Name": "Erick" , "NickName": "eum602", "Tasks": "develop" },
+		{"Name": "Alice" , "NickName": "al", "Tasks": "assist" }
+	]`) //MyJSON is a SLICE OF BYTES(uint8)
+
+	type Profile struct {
+		Name     string
+		NickName string
+		Tasks    string //if you declare with lowercase the variable is not shown
+	}
+
+	var profiles []Profile //also it could have been profiles := []Profile{}
+
+	err := json.Unmarshal(myJSON, &profiles)
+
+	if err != nil {
+		fmt.Println("error", err)
+	}
+
+	fmt.Printf("\n\n%v", profiles)
 }
