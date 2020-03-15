36fa7a6346a63f1d3e21a5d370975e2bd250fca0
Erick <eum602@gmail.com>
2020-03-14 23:44:21 -0500

    added example how to return a func() int from another function, similar to previous example

--- 115_returning_func/main.go
+++ 115_returning_func/main.go
@@ -13,12 +13,15 @@ func main() {
 	example2()
 
 	example3()
+
+	example4()
 }
 
 func definitions() {
 	fmt.Println(`
 	Functions are first class citizens in go. It is possible to return a function from a function, it works in the same
-	way as we return a string or something else.`)
+	way as we return a string or something else.
+	Function is a TYPE.`)
 }
 
 func example1() {
@@ -29,7 +32,7 @@ func example1() {
 }
 
 func example2() {
-	fmt.Println("Retuning a function that returns a type 'int'")
+	fmt.Println("\nRetuning a function that returns a type 'int'")
 	s1 := func() int {
 		return 345
 	}()
@@ -39,7 +42,7 @@ func example2() {
 }
 
 func example3() {
-	fmt.Println("Retuning a function that returns a type 'func() int'")
+	fmt.Println("\nRetuning a function that returns a type 'func() int'")
 	s1 := func() int {
 		return 345
 	}
@@ -48,8 +51,26 @@ func example3() {
 	fmt.Printf("%T", s1)
 }
 
+func example4() {
+	fmt.Println("\nUsed a declared function that returns a function() which in turn returns a type int")
+	s1 := bar()
+	fmt.Printf("%T", s1)
+
+	fmt.Println("\nNow EXECUTING my variable which has the function that returns an int")
+	a := s1()
+	fmt.Println("The returned value is", a)
+	a = bar()()
+	fmt.Println("because a=s1() and s1=bar() then a=bar()() =", a)
+}
+
 func foo() string {
 	s := "hello world"
 	return s
 
+}
+
+func bar() func() int {
+	return func() int {
+		return 123
+	}
 }
