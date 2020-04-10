8df578e90b7ffd60ce93d4645f37fce905af7dc2
Erick <eum602@gmail.com>
2020-04-10 17:28:03 -0500

    add example about how to make sure a file will be closed with 'defer' even after many returns and failures

--- 179_printing_logging/main.go
+++ 179_printing_logging/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
 func main() {
+
 	example1()
+	example3()
 	example2()
+}
+
+func definitions() {
+	fmt.Println(`
+	Defer statements allow us to think about closing each file right after opening it, guaranteeing that, 
+	regardless of the number of return statements in the function, the files will be closed.
+	`)
+
 }
 
 func example1() {
@@ -38,6 +49,38 @@ func example2() {
 	defer f2.Close()
 }
 
+func example3() {
+	f, err := os.Create("log1.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()  //closing at the end
+	log.SetOutput(f) //setting that all log information input will go to the log.txt file
+
+	written, err := CopyFile("copy", "log.txt")
+	if err != nil {
+		log.Println("Error in example 3", err)
+	}
+	log.Println("Successfull copy", written)
+}
+
 func deferred() {
 	fmt.Println("Deferred functions will not be executed when os.Exit(1) is invoked")
+}
+
+//CopyFile ...
+func CopyFile(dstName, srcName string) (written int64, err error) {
+	src, err := os.Open(srcName)
+	if err != nil {
+		return
+	}
+	defer src.Close()
+
+	dst, err := os.Create(dstName)
+	if err != nil {
+		return
+	}
+	defer dst.Close() //runs after return is executed. Guarantee even when errors happens the file will be closed.
+
+	return io.Copy(dst, src)
 }
