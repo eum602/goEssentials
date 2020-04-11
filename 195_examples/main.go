36a5ce10279a0844a4ee4156d5e5148466d5456a
Erick <eum602@gmail.com>
2020-04-11 08:20:21 -0500

    added some comments how to see this package documentation with 'godoc -http=:8080' and run the tests with 'go test ./...'

--- 195_examples/main.go
+++ 195_examples/main.go
@@ -1,5 +1,4 @@
-package main
-
+package main //to run all tests under this folder use go test ./... and to see the documentation use godoc -http=:8080
 import (
 	"fmt"
 	"github.com/eum602/goEssentials/195_examples/operations" //this package can be put not neccesarily here but on
